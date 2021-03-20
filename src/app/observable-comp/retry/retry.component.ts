2e23a9894e6107d1dc04c1f594ee277e8552caac
Rahul <rahulkr.mits@gmail>
2021-03-20 17:21:13 +0530

    retry done

--- src/app/observable-comp/retry/retry.component.ts
+++ src/app/observable-comp/retry/retry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-retry',
@@ -7,11 +7,28 @@ import { HttpClient} from '@angular/common/http'
   styleUrls: ['./retry.component.scss']
 })
 export class RetryComponent implements OnInit {
-
-  constructor(private _http:HttpClient) { }
+  person: any;
+  fetching: boolean = false;
+  status="No Data";
+  constructor(private _http: HttpClient) { }
 
   ngOnInit(): void {
-    
+  }
+  getData() {
+    this.fetching=true;
+    this._http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(res => {
+      
+      setTimeout(() => {
+        console.log(res);
+        this.person = res;
+        this.fetching=false;
+        this.status="Data Fetched";
+      }, 1500);
+    }, 
+    (err)=>{
+    this.fetching=false;
+    this.status="Problem in fetching data";
+    })
   }
 
 }
